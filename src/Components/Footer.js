a34ee82ce15d19c84fb73769eb2f22d2d320f1f7
Radioboxgit <chinedulawrence43@yahoo.com>
2022-10-10 18:39:59 +0100

    added copyright icon

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,6 @@
-import { FaGithub,FaLinkedinIn,FaTwitter } from 'react-icons/fa';
+import { FaGithub,FaLinkedinIn,FaTwitter,FaRegCopyright } from 'react-icons/fa';
 import styles from './Footer.module.css'
+
 
 export default function Footer(){
     return (
@@ -9,7 +10,7 @@ export default function Footer(){
                 <a href="https://www.linkedin.com/in/chinedu-anigbogu-62ab74188"><FaLinkedinIn style={ { color: "#6358fd","fontSize":"30px","margin":"3px"}}/> </a> 
                 <a href="https://github.com/Radioboxgit"><FaGithub style={ { color: "#bfbfc2","fontSize":"30px","margin":"3px"}} /> </a>          
             </div>  
-            <p>Copyright 2022 INFO-SPACE Powered By <a href="https://bio.link/codeclas" rel="noreferrer" target="_blank" > @codeclas </a> </p>        
+            <div className={styles.copy}> <FaRegCopyright style={ { color: "white","fontSize":"15px","margin":"2px"}}/>  Copyright 2022 INFO-SPACE Powered By <a href="https://bio.link/codeclas" rel="noreferrer" target="_blank" > @codeclas </a> </div>        
         </div>
     )
 }
